fb86349af45c0f71a0c368d82444c335f4dc9ffa
timdv91 <timdevlieg@gmail.com>
2017-10-03 15:54:01 +0200

    Some UI tweaks
    Adding manual connection textbox on UI, (not coded yet).
    Adding groupboxes.

--- FROST-configManager/Form1.Designer.cs
+++ FROST-configManager/Form1.Designer.cs
@@ -30,11 +30,18 @@
         {
             this.btnSendDiscoveryPing = new System.Windows.Forms.Button();
             this.listBox_DevicesDiscoverd = new System.Windows.Forms.ListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtManualConnectionIP = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSendDiscoveryPing
             // 
-            this.btnSendDiscoveryPing.Location = new System.Drawing.Point(13, 12);
+            this.btnSendDiscoveryPing.Location = new System.Drawing.Point(6, 19);
             this.btnSendDiscoveryPing.Name = "btnSendDiscoveryPing";
             this.btnSendDiscoveryPing.Size = new System.Drawing.Size(191, 41);
             this.btnSendDiscoveryPing.TabIndex = 0;
@@ -45,20 +52,74 @@
             // listBox_DevicesDiscoverd
             // 
             this.listBox_DevicesDiscoverd.FormattingEnabled = true;
-            this.listBox_DevicesDiscoverd.Location = new System.Drawing.Point(13, 60);
+            this.listBox_DevicesDiscoverd.Location = new System.Drawing.Point(9, 66);
             this.listBox_DevicesDiscoverd.Name = "listBox_DevicesDiscoverd";
             this.listBox_DevicesDiscoverd.Size = new System.Drawing.Size(585, 511);
             this.listBox_DevicesDiscoverd.TabIndex = 1;
+            this.listBox_DevicesDiscoverd.SelectedIndexChanged += new System.EventHandler(this.listBox_DevicesDiscoverd_SelectedIndexChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Enabled = false;
+            this.btnConnect.Location = new System.Drawing.Point(400, 19);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(191, 41);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Manual connection using IP adress:";
+            // 
+            // txtManualConnectionIP
+            // 
+            this.txtManualConnectionIP.Location = new System.Drawing.Point(9, 45);
+            this.txtManualConnectionIP.Name = "txtManualConnectionIP";
+            this.txtManualConnectionIP.Size = new System.Drawing.Size(585, 20);
+            this.txtManualConnectionIP.TabIndex = 4;
+            this.txtManualConnectionIP.TextChanged += new System.EventHandler(this.txtManualConnectionIP_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSendDiscoveryPing);
+            this.groupBox1.Controls.Add(this.listBox_DevicesDiscoverd);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 596);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Auto discovery:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtManualConnectionIP);
+            this.groupBox2.Location = new System.Drawing.Point(13, 614);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(605, 82);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Manual connection (Advanced users only!):";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1069, 660);
-            this.Controls.Add(this.listBox_DevicesDiscoverd);
-            this.Controls.Add(this.btnSendDiscoveryPing);
+            this.ClientSize = new System.Drawing.Size(631, 706);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +128,11 @@
 
         private System.Windows.Forms.Button btnSendDiscoveryPing;
         private System.Windows.Forms.ListBox listBox_DevicesDiscoverd;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtManualConnectionIP;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- FROST-configManager/Form1.cs
+++ FROST-configManager/Form1.cs
@@ -17,10 +17,30 @@ namespace FROST_configManager
             InitializeComponent();
         }
 
+        //Starts a discovery for devices on local network:
         private void btnSendDiscoveryPing_Click(object sender, EventArgs e)
         {
             autoDiscovery ad = new autoDiscovery();
             listBox_DevicesDiscoverd.DataSource = ad.getAllFROSTDevices(70);
+            
+        }
+
+        //event when index of listbox changes, enables btnConnect:
+        private void listBox_DevicesDiscoverd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox_DevicesDiscoverd.SelectedItems.Count > 0)
+                btnConnect.Enabled = true;
+            else
+                btnConnect.Enabled = false;
+        }
+
+        //event when manualconnection textbox input gets larger then 5, enables btnConnect:
+        private void txtManualConnectionIP_TextChanged(object sender, EventArgs e)
+        {
+            if (txtManualConnectionIP.Text.Length > 5)
+                btnConnect.Enabled = true;
+            else
+                btnConnect.Enabled = false;
         }
     }
 }
