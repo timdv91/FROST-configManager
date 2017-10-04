44ed50dde1518421bfb24b345bf9fe9c4c146686
timdv91 <timdevlieg@gmail.com>
2017-10-05 00:42:13 +0200

    Reupload again, probably VS bugging...
    Urch...

--- FROST-configManager/configManagerForm.Designer.cs
+++ FROST-configManager/configManagerForm.Designer.cs
@@ -40,7 +40,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(1541, 12);
+            this.btnExit.Location = new System.Drawing.Point(12, 57);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 39);
             this.btnExit.TabIndex = 0;
@@ -51,7 +51,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Location = new System.Drawing.Point(99, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 1;
@@ -59,7 +59,7 @@
             // 
             // richTextBox_DeviceInformation
             // 
-            this.richTextBox_DeviceInformation.Location = new System.Drawing.Point(16, 28);
+            this.richTextBox_DeviceInformation.Location = new System.Drawing.Point(102, 22);
             this.richTextBox_DeviceInformation.Name = "richTextBox_DeviceInformation";
             this.richTextBox_DeviceInformation.ReadOnly = true;
             this.richTextBox_DeviceInformation.Size = new System.Drawing.Size(337, 160);
@@ -69,7 +69,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(359, 28);
+            this.label2.Location = new System.Drawing.Point(99, 200);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 3;
@@ -77,14 +77,14 @@
             // 
             // txtDeviceName
             // 
-            this.txtDeviceName.Location = new System.Drawing.Point(438, 25);
+            this.txtDeviceName.Location = new System.Drawing.Point(178, 197);
             this.txtDeviceName.Name = "txtDeviceName";
-            this.txtDeviceName.Size = new System.Drawing.Size(244, 20);
+            this.txtDeviceName.Size = new System.Drawing.Size(261, 20);
             this.txtDeviceName.TabIndex = 4;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(1460, 12);
+            this.btnSave.Location = new System.Drawing.Point(12, 12);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 39);
             this.btnSave.TabIndex = 5;
@@ -94,15 +94,15 @@
             // 
             // txtMeasurmentInterval
             // 
-            this.txtMeasurmentInterval.Location = new System.Drawing.Point(525, 67);
+            this.txtMeasurmentInterval.Location = new System.Drawing.Point(265, 226);
             this.txtMeasurmentInterval.Name = "txtMeasurmentInterval";
-            this.txtMeasurmentInterval.Size = new System.Drawing.Size(157, 20);
+            this.txtMeasurmentInterval.Size = new System.Drawing.Size(174, 20);
             this.txtMeasurmentInterval.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(359, 70);
+            this.label3.Location = new System.Drawing.Point(99, 229);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(160, 13);
             this.label3.TabIndex = 7;
@@ -112,7 +112,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1628, 893);
+            this.ClientSize = new System.Drawing.Size(478, 289);
             this.ControlBox = false;
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtMeasurmentInterval);
